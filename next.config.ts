31cc51888b6ac8c42ec34da7da4d80c53614031a
MannLester <mannlesterm@gmail.com>
2025-03-27 15:32:47 +0800

    Vercel Ready

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'standalone',
+    images: {
+        unoptimized: true, // This is recommended for static exports
+        domains: [], // Add any external image domains here if needed
+    },
+    reactStrictMode: true,
+    swcMinify: true,
 };
 
 export default nextConfig;
