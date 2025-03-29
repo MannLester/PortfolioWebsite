b5841a064dbc9877eb7384749665eb84f355f3bc
MannLester <mannlesterm@gmail.com>
2025-03-29 12:19:08 +0800

    Design Changes

--- src/components/sections/HomePage.tsx
+++ src/components/sections/HomePage.tsx
@@ -221,10 +221,10 @@ const HomePage = () => {
 
           {/* Red glow effects */}
           <div className="absolute inset-0 -z-10 overflow-hidden pointer-events-none">
-            <div className="absolute top-1/4 right-1/4 w-96 h-96 bg-[#9933ff]/10 rounded-full blur-[100px]" />
-            <div className="absolute bottom-1/4 left-1/4 w-96 h-96 bg-[#9933ff]/5 rounded-full blur-[100px]" />
-            <div className="absolute top-1/4 right-1/4 w-96 h-96 bg-[#9933ff]/20 rounded-full blur-[150px]" />
-            <div className="absolute bottom-1/4 left-1/4 w-96 h-96 bg-[#9933ff]/10 rounded-full blur-[150px]" />
+            <div className="absolute top-1/4 right-1/4 w-96 h-96 bg-red-500/10 rounded-full blur-[100px]" />
+            <div className="absolute bottom-1/4 left-1/4 w-96 h-96 bg-red-500/5 rounded-full blur-[100px]" />
+            <div className="absolute top-1/4 right-1/4 w-96 h-96 bg-red-500/20 rounded-full blur-[150px]" />
+            <div className="absolute bottom-1/4 left-1/4 w-96 h-96 bg-red-500/10 rounded-full blur-[150px]" />
           </div>
         </div>
       </section>
